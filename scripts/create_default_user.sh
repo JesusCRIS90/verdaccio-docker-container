353583ee638f2ad39ee31555fb2dc1b3d60220d6
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-11-20 18:04:35 +0100

    [MOD] Updated bash scripts

--- scripts/create_default_user.sh
+++ scripts/create_default_user.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 set -e
 
+# ENV_FILE="/home/jesus/git/verdaccio-docker-container/.env"
+
+# if [ -f "$ENV_FILE" ]; then
+#   echo "Loading environment variables from $ENV_FILE..."
+#   export $(grep -v '^#' "$ENV_FILE" | xargs)
+# else
+#   echo ".env file not found at $ENV_FILE. Exiting."
+#   exit 1
+# fi
+
 # Start Verdaccio in the background
 verdaccio &
 
 # Wait for Verdaccio to be up and running
-while ! wget -q --spider http://localhost:4873/; do
+while ! wget -q --spider http://localhost:${HOST_PORT}/; do
     echo "Waiting for Verdaccio to start..."
     sleep 5
 done
@@ -14,7 +24,7 @@ done
 if [ ! -z "$VERDACCIO_DEFAULT_USER_NAME" ] && [ ! -z "$VERDACCIO_DEFAULT_USER_PASSWORD" ] && [ ! -z "$VERDACCIO_DEFAULT_USER_EMAIL" ]; then
     echo "Creating default user: $VERDACCIO_DEFAULT_USER_NAME"
     npm adduser \
-        --registry http://localhost:4873 \
+        --registry http://localhost:${HOST_PORT} \
         --username "$VERDACCIO_DEFAULT_USER_NAME" \
         --password "$VERDACCIO_DEFAULT_USER_PASSWORD" \
         --email "$VERDACCIO_DEFAULT_USER_EMAIL"
--- start_verdaccio.sh
+++ start_verdaccio.sh
@@ -11,27 +11,58 @@ fi
 
 # Step 2: Stopping and Removing Verdaccio Container
 echo "Stopping and Deleting Verdaccio Container with Docker Compose..."
-docker-compose down
+docker-compose down -v
 
-# Step 3: Ensure scripts have executable permissions
+# Step 3: Optionally recreate verdaccio struct folder
+if [ "$RECREATE_STRUCT_FOLDER" = "TRUE" ]; then
+  echo "Deleting Verdaccio Host Struct Folder"
+  sudo rm -rf ./verdaccio
+  sleep 3
+else
+  echo "Skipping Deleting Verdaccio Struct Folder."
+fi
+
+# Step 4: Ensure scripts have executable permissions
 echo "Ensuring all scripts have executable permissions..."
 chmod +x ./scripts/create_verdaccio_struct.sh
 chmod +x ./scripts/copy_config.sh
 chmod +x ./scripts/create_default_user.sh
 
-# Step 4: Create the Verdaccio folder structure
+# Step 5: Create the Verdaccio folder structure
 echo "Creating Verdaccio folder structure..."
 ./scripts/create_verdaccio_struct.sh
 
-# Step 5: Copy and move the configuration file
+# Step 6: Copy and move the configuration file
 echo "Copying and moving the config file..."
 ./scripts/copy_config.sh
 
-# Step 6: Start the Verdaccio container
+# Step 7: Start the Verdaccio container
 echo "Starting Verdaccio container with Docker Compose..."
 docker-compose up -d
 
-# Step 7: Optionally create a default user inside the running container
+# Step 8: Adjust permissions and ownership for Verdaccio directories
+echo "Modifying folder permissions and ownership..."
+
+# Check if directories exist and create them if necessary
+adjust_permissions() {
+  local dir=$1
+  echo "Processing $dir..."
+
+  if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
+    echo "Created $dir"
+  fi
+
+  # Ensure proper ownership for the Verdaccio user
+  sudo chown -R 10001:10001 "$dir"
+  sudo chmod -R 777 "$dir"
+}
+
+adjust_permissions ./verdaccio/storage
+adjust_permissions ./verdaccio/config
+adjust_permissions ./verdaccio/plugins
+
+# Step 9: Optionally create a default user inside the running container
 if [ "$CREATE_DEFAULT_USER" = "TRUE" ]; then
   echo "Creating default user inside the container..."
   docker exec verdaccio /scripts/create_default_user.sh
@@ -40,6 +71,3 @@ else
 fi
 
 echo "Verdaccio setup is complete. Access it at http://localhost:${HOST_PORT:-4873}"
-
-
-
